9f8f2ffd4d5d89946195b7057ab3ae70dc2ee5fd
Ken Driscoll <driscoll.ken@gmail.com>
2015-06-28 18:50:46 -0500

    Halfway through lesson 3b.  Able to play normal sample audio from the snail button.

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Ken Driscoll on 6/28/15.
+//  Copyright (c) 2015 Ken Driscoll. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    var audioPlayer:AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+        } else {
+            println("the file is not found")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func playSnailAudio(sender: AnyObject) {
+        // Play audio sloooooowly here....
+        audioPlayer.play()
+    }
+}
--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        // Hide the stop button
+        stopButton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +34,15 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Record the user's voice
         recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
     }
 
     
     @IBAction func stopRecording(sender: UIButton) {
         recordingInProgress.hidden = true
+        stopButton.hidden = true
+        recordButton.enabled = true
     }
 }
 
